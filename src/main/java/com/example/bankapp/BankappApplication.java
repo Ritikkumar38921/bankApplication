787b790aca8984bfdfe5aae9cfaea41d671459a1
Ritik kumar <thakurritik38921@gmail.com>
2025-04-04 09:44:55 +0530

    chnages in the file

--- src/main/java/com/example/bankapp/BankappApplication.java
+++ src/main/java/com/example/bankapp/BankappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Ritik");
 		SpringApplication.run(BankappApplication.class, args);
 	}
 
